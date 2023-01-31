0e59b979f9bf2cf36f8b2c68a041c9b5db9671d2
sapsu <113932803+sapsu@users.noreply.github.com>
2023-01-31 09:58:48 +0100

    Added dictionary API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -6,12 +6,12 @@ export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
   function handleResponse(response) {
-    console.log(response.data);
+    console.log(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for the definition of ${keyword}`);
+
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
