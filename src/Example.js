5ea1d1380fb4010aa424474be1f2553ed91a2c16
sapsu <113932803+sapsu@users.noreply.github.com>
2023-02-01 10:51:45 +0100

    Added new component for word phonetics + fixed a bug in Meaning.jsin

--- src/Example.js
+++ src/Example.js
@@ -3,10 +3,10 @@ import React from "react";
 export default function Example(props) {
   if (props.example) {
     return (
-      <p>
+      <div>
         <strong>Example: </strong>
         <em>{props.example}</em>
-      </p>
+      </div>
     );
   } else {
     return null;
--- src/Meaning.js
+++ src/Meaning.js
@@ -3,21 +3,20 @@ import Synonyms from "./Synonyms";
 import Example from "./Example";
 
 export default function Meaning(props) {
-  console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>
+            <div>
               <strong>Definition: </strong>
               {definition.definition}
               <br />
               <Example example={definition.example} />
               <br />
               <Synonyms synonyms={definition.synonyms} />
-            </p>
+            </div>
           </div>
         );
       })}
